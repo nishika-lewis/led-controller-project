5b823d2df8328f5eb07464f29a672dc032fc302b
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2022-11-22 16:31:22 -0500

    Changed Wi-Fi settings/moved connect code to main

--- pico/connect.py
+++ pico/connect.py
@@ -1,24 +1,29 @@
-# Read settings.json
-from lib.at_client import io_util
-ssid, password, atSign = io_util.read_settings()
-del io_util  # Make space in memory
+def main():
+    # Read settings.json
+    from lib.at_client import io_util
+    ssid, password, atSign = io_util.read_settings()
+    del io_util  # Make space in memory
 
-# Connect to wifi
-from lib import wifi
-print(f'Connecting to WiFi {ssid}...')
-wifi.init_wlan(ssid, password)
-del ssid, password, wifi  # Make space in memory
+    # Connect to wifi
+    from lib import wifi
+    print(f'Connecting to WiFi {ssid}...')
+    wifi.init_wlan(ssid, password)
+    del ssid, password, wifi  # Make space in memory
 
-# Connect and pkam authenticate into secondary
-from lib.at_client import at_client
-atClient = at_client.AtClient(atSign, writeKeys=False)
-atClient.pkam_authenticate(verbose=True)
-del at_client  # Make space in memory
+    # Connect and pkam authenticate into secondary
+    from lib.at_client import at_client
+    atClient = at_client.AtClient(atSign, writeKeys=False)
+    atClient.pkam_authenticate(verbose=True)
+    del at_client  # Make space in memory
 
-# Receive data from app
-key = 'instructions'
-appAtSign = '@computer0'
+    # Receive data from app
+    key = 'instructions'
+    appAtSign = '@computer0'
 
-while True:
-    data = atClient.get_public(key, appAtSign)
-    print(data)
+    # Print the data forever
+    while True:
+        data = atClient.get_public(key, appAtSign)
+        print(data)
+
+if __name__ == '__main__':
+    main()
