595dd79ad3d1cf80381d1cc83089568dc8561b2d
nishikalewis <113398001+nishikalewis@users.noreply.github.com>
2022-11-22 16:53:03 -0500

    Added user interface

--- java_app/src/main/java/io/github/ledproject/App.java
+++ java_app/src/main/java/io/github/ledproject/App.java
@@ -1,5 +1,7 @@
 package io.github.ledproject;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import org.atsign.client.api.AtClient;
 import org.atsign.common.AtSign;
 import org.atsign.common.KeyBuilders;
@@ -7,17 +9,26 @@ import org.atsign.common.Keys.PublicKey;
 
 public class App
 {
-    public static void main(String[] args) throws Exception
+    public static void main(String args[])
+    {
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
+    }
+
+    public static void connect() throws Exception
     {
         AtSign atSign = new AtSign("@computer0");
         AtClient atClient = AtClient.withRemoteSecondary(atSign);
         PublicKey pk = new KeyBuilders.PublicKeyBuilder(atSign).key("instructions").build();
         int instruction = 0;
-        
-        while (true)
-        {
+
+        while (true) {
             Thread.sleep(5000);
-            String value = "" + instruction++;
+            String value = "" + instruction;
             String response = atClient.put(pk, value).get();
             System.out.println(response);
         }
